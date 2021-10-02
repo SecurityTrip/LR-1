fce2217adb8c3f2c5a48620b9b0ad77fe3eac533
Илья Лысов <Lysovidq@gmail.com>
2021-10-02 17:46:02 +0400

    v4.1 #todo -int

--- Source.cpp
+++ Source.cpp
@@ -12,26 +12,26 @@ int main() {
 		double input_value;   // инициализация входного значения
 		cin >> input_value; // ввод значения
 
-	goBackToInput://метка возврата проверки ввода
+	
 
-		if (cin.fail() || input_value < 0 || (input_value - int(input_value) != 0.0)) // проверка ввода
+		while ((cin.fail()) || (cin.peek() != '\n') || (input_value - int(input_value) != 0.0)) // проверка ввода
 		{
 			cin.clear(); // возврат cin в обычный режим работы
-			cin.ignore(32767, '\n'); // удаление значения предыдущего ввода из входного буфера
+			cin.ignore(9999999, '\n'); // удаление значения предыдущего ввода из входного буфера
 			cout << "Wrong type of input data, try again" << endl;
 			
 			cout << "Input an integer: ";
 			cin >> input_value; // ввод значения
-			
-			goto goBackToInput; //везврат к метке проверки ввода
 		}
-		else
+		
+		int processed_value;
+		processed_value = int(input_value);
+
+		if (processed_value >= 0) //перевод не отрицательных чисел в двоичную систему счисления
 		{
-			int processed_value;
-			processed_value = int(input_value);
-			if (processed_value != 1) {
+			if (processed_value > 1)
+			{
 				string output_bin; //инициализация выводного  значения
-			EscapeFromProcessing:
 				while (processed_value > 1)
 				{
 					output_bin += to_string(processed_value % 2);
@@ -39,7 +39,6 @@ int main() {
 					if (processed_value == 1)
 					{
 						output_bin += to_string(processed_value);
-						goto EscapeFromProcessing;
 					}
 				}
 
@@ -52,34 +51,38 @@ int main() {
 			}
 			else
 			{
-				cout << "Result: " << processed_value<< endl;
+				cout << "Result: " << processed_value << endl;
 			}
-			
 		}
+		else //перевод отрицательных чисел в двоичную систему счисления
+		{
 
+		}
+		
 		cout << "Continue? (Y/N) " << endl;
 		string end_point;  //переменная для завершения выполнения при вводе N
 		cin >> end_point;
 		cout << endl;
 
-
-		while (end_point != "Y" and end_point != "N")//проверка ввода Y/N
+		while (true)
 		{
-			cout << "Unidentified operator, try again" << endl;
-			cout << "Continue? (Y/N) " << endl;
-			cin >> end_point;
-			cout << endl;
-			if (end_point == "Y" || end_point == "N")
+			if ((end_point == "Y") || (end_point == "y") || (end_point == "N") || (end_point == "n"))
 			{
 				break;
 			}
+			else
+			{
+				cout << "Unidentified operator, try again" << endl;
+				cout << "Continue? (Y/N) " << endl;
+				cin >> end_point;
+				cout << endl;
+			}
 		}
 
-		if (end_point == "N")
+		if (end_point == "N" || end_point == "n")
 		{
 			break;
 		}
-
 	}
 	return 0;
 }
